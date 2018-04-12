b2992a287699a16bf8cdcb82d6e59143f94b6c99
Amy Tu <siamesebb@gmail.com>
2018-04-12 03:59:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,11 +2,3 @@ var playlist = {
   'Mozart':'Requiem',
   'Albanini':'Adagio'
 }
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle
-  return playlist
-}
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName]
-  return playlist
-}
